789937c7bf9556d0932f3be92a8344b5865b839e
DenisVlasiuk <fcdenisvlasiuk@gmail.com>
2022-07-23 23:27:51 +0300

    Initial commit

---
+++ src/test/java/DemoTest.java
@@ -0,0 +1,13 @@
+public class DemoTest {
+
+    static int sosiski = 50;
+
+    public static void main(String[] args) {
+
+        if (sosiski > 70) {
+            System.out.println("Akcija, bistro pokupaem sosiski!");}
+        else {
+            System.out.println("Net akcui, proxodim mimo");}
+
+    }
+}
